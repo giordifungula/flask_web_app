979cbefb00664be98d44c34ff34b1ef8f17cb8f4
giordifungula <jodi.fungula@gmail.com>
2019-10-19 18:45:01 +0300

    class added and set up model for database

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import flask_sqlalchemy;
+
+ENV = 'dev'
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postresql://postgress:127.0.0.1@localhost/lexus'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_DATABASE_URI'] = False
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Models):
+    __tablename = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.Text(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
